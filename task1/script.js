ee711ce402531175a0c31586a78217d237832dd6
born_to_chill <born_to_chill@likeapro.ru>
2024-01-25 16:48:15 +0300

    testExercise.nativeJS

--- task1/script.js
+++ task1/script.js
@@ -1,20 +1,45 @@
-$(document).ready(function () {
-  // скрытие заголовка
-  $(".btn-warning").click("click", function () {
-    $(".row-top").slideToggle(500);
-  });
-  // перемещение среднего блока
-  $(".btn-success").click("click", function () {
-    $(".column-center").toggleClass("order-first");
-  });
-  // модальное окно
-  $(".popup-close").click(function () {
-    $(".popup-fade").hide(500);
-  });
+// $(document).ready(function () {
+//   // скрытие заголовка
+//   $(".btn-warning").click("click", function () {
+//     $(".row-top").slideToggle(500);
+//   });
+//   // перемещение среднего блока
+//   $(".btn-success").click("click", function () {
+//     $(".column-center").toggleClass("order-first");
+//   });
+//   // модальное окно
+//   $(".popup-close").click(function () {
+//     $(".popup-fade").hide(500);
+//   });
 
-  $(".popup-fade").click(function (e) {
-    if ($(e.target).closest(".popup")) {
-      $(this).fadeOut();
-    }
-  });
+//   $(".popup-fade").click(function (e) {
+//     if ($(e.target).closest(".popup")) {
+//       $(this).fadeOut();
+//     }
+//   });
+// });
+
+const titleBlock = document.querySelector(".row-top");
+const columnCenter = document.querySelector(".column-center");
+const popupBlock = document.querySelector(".popup-fade");
+const btnHideTitle = document.querySelector(".btn-warning");
+const btnSwap = document.querySelector(".btn-success");
+const btnClose = document.querySelector(".popup-close");
+
+btnHideTitle.addEventListener("click", function () {
+  titleBlock.classList.toggle("hidden");
+});
+
+btnSwap.addEventListener("click", function () {
+  columnCenter.classList.toggle("order-first");
+});
+
+btnClose.addEventListener("click", function () {
+  popupBlock.classList.add("hidden");
+});
+
+popupBlock.addEventListener("click", function (e) {
+  if (e.target == popupBlock) {
+    popupBlock.classList.add("hidden");
+  }
 });
--- task2/script.js
+++ task2/script.js
@@ -1,27 +1,53 @@
-$(document).ready(function () {
-  let $data = {};
+// $(document).ready(function () {
+//   let $data = {};
 
-  let renderData = function (data) {
-    let text = JSON.stringify(data);
-    $(`<p>${text}</p>`).appendTo("body");
-  };
+//   let renderData = function (data) {
+//     let text = JSON.stringify(data);
+//     $(`<p>${text}</p>`).appendTo("body");
+//   };
 
-  $(".form").submit(function (e) {
-    e.preventDefault();
-    $(".form")
-      .find("input, select")
-      .each(function () {
-        $data[this.name] = $(this).val();
-      });
-    renderData($data);
+//   $(".form").submit(function (e) {
+//     e.preventDefault();
+//     $(".form")
+//       .find("input, select")
+//       .each(function () {
+//         $data[this.name] = $(this).val();
+//       });
+//     renderData($data);
 
-    $.ajax({
-      type: "GET",
-      url: "./index.html",
-      data: $data,
-      success: function () {
-        alert("Успех! 🥳 🥳");
-      },
-    });
-  });
+//     $.ajax({
+//       type: "GET",
+//       url: "./index.html",
+//       data: $data,
+//       success: function () {
+//         alert("Успех! 🥳 🥳");
+//       },
+//     });
+//   });
+// });
+
+const form = document.querySelector(".form");
+const list = document.querySelector(".list");
+
+const renderItem = function (item) {
+  const markup = `
+    <li class="list__item">
+    ${item}
+    </li>
+    `;
+  list.insertAdjacentHTML("beforeend", markup);
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const dataArr = new FormData(form);
+  const dataObj = Object.fromEntries(dataArr);
+  const text = JSON.stringify(dataObj);
+  renderItem(text);
+  getResponse();
 });
+
+async function getResponse() {
+  const response = await fetch("./index.html");
+  if (response.ok) alert("good");
+}
